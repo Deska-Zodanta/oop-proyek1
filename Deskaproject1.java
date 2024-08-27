9c0f1bd711ac717e33e60e6845aea8807381807b
Deska-Zodanta <zodandeska@gmail.com>
2024-08-28 02:52:28 +0700

    first commit

---
+++ Deskaproject1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.deskaproject1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Deskaproject1 {
+
+    public static void main(String[] args) {
+        String nama = "Deska Zodanta";
+        String npm = "2310506014";
+        String alamat = "Magelang";
+        String telp = "085875473098";
+        System.out.println("Nama : "+nama);
+        System.out.println("NPM : "+npm);
+        System.out.println("Alamat : "+alamat);
+        System.out.println("No.Telp : "+telp);
+    }
+}
